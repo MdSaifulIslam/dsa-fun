7a1fd965bc38d1fe2afbbefe3d8b9e3e05c07dd4
MdSaifulIslam <mohammadsaifulcsepstu@gmail.com>
2026-06-01 17:28:18 +0900

    - reverse single list

--- linkedList01.c
+++ linkedList01.c
@@ -33,23 +33,62 @@ void printList(struct Node *list)
     struct Node *temp = list;
     while (temp != NULL)
     {
-        printf("%d\t", temp->data);
+        printf("%d", temp->data);
         temp = temp->next;
+        temp == NULL ? printf("") : printf("-->");
     }
+    printf("\n----------------------------------------\n");
+}
+
+// Avoided because of some redundent code
+// void reverseList(struct Node **list)
+// {
+//     if ((*list) == NULL || (*list)->next == NULL)
+//     {
+//         return;
+//     }
+
+//     struct Node *previous = NULL, *current = (*list), *next = current->next, *temp = NULL;
+//     while (next != NULL)
+//     {
+//         current->next = previous;
+//         previous = current;
+//         temp = next->next;
+//         next->next = current;
+//         current = next;
+//         next = temp;
+//     }
+//     (*list) = current;
+//     return;
+// }
+
+void reverseList(struct Node **list)
+{
+    struct Node *pre = NULL, *current = (*list);
+    while (current != NULL)
+    {
+        struct Node *next = current->next;
+        current->next = pre;
+        pre = current;
+        current = next;
+    }
+    *list = pre;
 }
 
 int main()
 {
-
     struct Node *head = NULL;
     struct Node *tail = NULL;
 
     int i = 1;
 
-    for (i = 1; i < 100; i ++){
+    for (i = 1; i < 1; i++)
+    {
         createList(&head, &tail, i);
     }
 
+    printList(head);
+    reverseList(&head);
     printList(head);
 
     return 0;
